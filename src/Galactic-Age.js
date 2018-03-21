626c1b30899a912dd73faa19209533d62007834d
Guest User <Guest@Epicodus-5B.local>
2018-03-21 15:04:46 -0700

    add mars and jupiter methods to convert earth years to mars and jupiter years and test it.

--- src/Galactic-Age.js
+++ src/Galactic-Age.js
@@ -16,11 +16,11 @@ export class Date{
   venusAge(){
    return this.userAge * 1.6; // revolution period: 365 earth days divided by 224.7 venus days = 1.6
   }
-  // marsAge(){
-  //   return this.userAge * .53129548762;// revolution period: 365 earth days divided by 224.7 venus days = .53129548762
-  //
-  // }
-  // jupiterAge(){
-  //   return this.userAge * .00707364341;// revolution period: 365 earth days divided by 51600 jupiter days = .00707364341
-  // }
+  marsAge(){
+    return this.userAge * .53129548762;// revolution period: 365 earth days divided by 224.7 venus days = .53129548762
+
+  }
+  jupiterAge(){
+    return this.userAge * .00707364341;// revolution period: 365 earth days divided by 51600 jupiter days = .00707364341
+  }
 }
